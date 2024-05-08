c3cb18485bcb3b19a58ac0013de51cf7d6450200
Sanjin Dedic <ozrobotix@gmail.com>
2024-05-09 09:57:38 +1000

    Update capsule_creator.py

--- 2024_STAGE_1/capsule_creator.py
+++ 2024_STAGE_1/capsule_creator.py
@@ -3,7 +3,6 @@ import time
 import os
 import requests
 
-caesar_url = 'https://vccfinal.net/time_capsule/caesar_encrypt'
 vigenere_url = 'https://vccfinal.net/time_capsule/vigenere_encrypt'
 
 def create_time_capsule():
@@ -28,52 +27,25 @@ def create_time_capsule():
         print("Invalid locking option. Defaulting to 10 days.")
         lock_duration = 10 * 24 * 60 * 60  # 10 days in seconds
 
-    print("\nEncryption Methods:")
-    print("1. Caesar Cipher")
-    print("2. Vigenère Cipher")
-    encryption_option = input("Choose the encryption method (1-2): ")
-
-    if encryption_option == "1":
-        data = {
-            'message': message,
-            'key': user_key
-        }
-        response = requests.post(caesar_url, json=data)
-        if response.status_code == 200:
-            encrypted_message = response.json()['encrypted_message']
-        else:
-            print("An error occurred.")
-        encryption_method = "caesar"
-    elif encryption_option == "2":
-        data = {
-            'message': message,
-            'key': user_key
-        }
-        response = requests.post(vigenere_url, json=data)
-        if response.status_code == 200:
-            encrypted_message = response.json()['encrypted_message']
-        else:
-            print("An error occurred.")
-
-        encryption_method = "vigenere"
+    data = {
+        'message': message,
+        'key': user_key
+    }
+    response = requests.post(vigenere_url, json=data)
+    if response.status_code == 200:
+        encrypted_message = response.json()['encrypted_message']
+        print("encrypted message:", encrypted_message)
     else:
-        print("Invalid encryption option. Defaulting to Caesar Cipher.")
-        data = {
-            'message': message,
-            'key': user_key
-        }
-        response = requests.post(caesar_url, json=data)
-        if response.status_code == 200:
-            encrypted_message = response.json()['encrypted_message']
-        else:
-            print("An error occurred.")
-        encryption_method = "caesar"
+        print("An error occurred.")
+        return
+
+    encryption_method = "vigenere"
 
     time_created = int(time.time())
     time_revealed = time_created + lock_duration
     locked_by = input("Enter your name: ")
 
-    time_capsule_data =(
+    time_capsule_data = (
         f"message: {encrypted_message}, "
         f"time_created: {time_created}, "
         f"time_revealed: {time_revealed}, "
